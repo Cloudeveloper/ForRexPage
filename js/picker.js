5ddaf0c7fc7768679c1033f7184f8947d6086b9b
WaveTsuname <71974743+Cloudeveloper@users.noreply.github.com>
2022-02-13 12:02:02 +0000

    Update picker.js

--- js/picker.js
+++ js/picker.js
@@ -1,1 +1,9 @@
+var things = [
+  'you are very pretty'
+  'you are beautiful'
+]
 
+function newthings() {
+  var randomNumber = Math.floor(Math.random() * (things.length));
+  document.getElementById('thingDisplay').innerHTML - quotes[randomNumber];
+}
